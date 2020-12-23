8fa22cd8d6ee3d8b5710b0963bd031272bf3fd03
park <pcugogo@gmail.com>
2020-12-24 02:45:55 +0900

    appCoordinator 지역 변수로 변경

--- Example/SCoordinator/AppDelegate.swift
+++ Example/SCoordinator/AppDelegate.swift
@@ -13,11 +13,10 @@ import SCoordinator
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var appCoordinator: AppCoordinator!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        appCoordinator = AppCoordinator(root: window!)
+        let appCoordinator = AppCoordinator(root: window!)
         appCoordinator.navigate(to: AppRoute.main)
         
         return true
