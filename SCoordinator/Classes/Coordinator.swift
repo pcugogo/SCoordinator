3ca3911d7dae29685a0a810caf45396de11ca4fb
park <pcugogo@gmail.com>
2020-12-25 02:31:17 +0900

    코디네이터 메모리 관리 방식 변경

--- SCoordinator/Classes/Coordinator.swift
+++ SCoordinator/Classes/Coordinator.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 
-public typealias Coordinator<Root: RootType> = BaseCoordinator<Root> & CoordinatorType
-
-open class BaseCoordinator<Root: RootType> {
-    public unowned var root: Root!
+class Coordinator<RootView: RootViewType>: CoordinatorType, ParentCoordinator {
     
-    public init(root: Root) {
-        self.root = root
+    unowned var rootView: RootView
+    unowned var parent: ParentCoordinator!
+    
+    var childrens: [String:CoordinatorType] = [:]
+    
+    
+    init(rootView: RootView) {
+        self.rootView = rootView
+    }
+    
+    func start(with parentCoordinator: ParentCoordinator) {
+        parent = parentCoordinator
+        let childKey = String(describing: Self.self)
+        parent.childrens[childKey] = self
+    }
+    
+    func navigate(route: Route) {}
+    
+    func end(type: EndType) {
+        switch type {
+        case let .dismiss(animated, completion):
+            childrens.removeValue(forKey: String(describing: Self.self))
+            if let viewController = rootView as? UIViewController {
+                viewController.dismiss(animated: animated, completion: completion)
+            } else {
+                NSLog("ERROR: - The RootView of **\(String(describing: Self.self))** is not a UIViewController type.")
+            }
+        case let .popView(animated):
+            if let navigationController = rootView as? UINavigationController {
+                navigationController.popViewController(animated: animated)
+            } else {
+                NSLog("ERROR: - The RootView of **\(String(describing: Self.self))** is not a UINavigationController type.")
+            }
+        }
     }
 }
