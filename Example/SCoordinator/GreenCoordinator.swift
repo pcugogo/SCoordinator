6349a2e2c2896658a42287721bd1e9ea450629d0
park <pcugogo@gmail.com>
2021-01-05 23:03:48 +0900

    불필요한 코드 제거

--- Example/SCoordinator/GreenCoordinator.swift
+++ Example/SCoordinator/GreenCoordinator.swift
@@ -12,9 +12,6 @@ import SCoordinator
 final class GreenCoordinator : Coordinator<UIViewController> {
     
     override func navigate(to route: Route) {}
-    func end(animated: Bool, completion: (() -> Void)?) {
-        rootView.dismiss(animated: animated, completion: completion)
-    }
     
     deinit {
         print("\(String(describing: Self.self)) deinit")
